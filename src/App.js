8dadbf4b19d3a925d8d08aca05e5c43fe864de3e
wojtko <j.w2001@onet.pl>
2023-08-27 17:38:09 +0200

    ADD: Favicon and main container's styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { styled } from "styled-components";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <AppContainer>
+      <AppContent>
+        {/* <Sidebar></Sidebar>
+        <Chat></Chat> */}
+      </AppContent>
+    </AppContainer>
+  );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  display: grid;
+  place-items: center;
+  background-color: #0c1317;
+  height: 100vh;
+`;
+
+const AppContent = styled.div`
+  display: flex;
+  height: 95vh;
+  width: 95vw;
+  box-shadow: -1px 4px 20px -6px rgba(0, 0, 0, 0.7);
+`;
