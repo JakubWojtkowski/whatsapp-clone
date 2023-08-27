418c8564b77334d1cb5418a2610f1554d29478b8
wojtko <j.w2001@onet.pl>
2023-08-27 16:18:44 +0200

    ADD: Firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyV_ZWe6VAKF7cEm3affmXHIxDCevDnwk",
+  authDomain: "whatsapp-clone-96181.firebaseapp.com",
+  projectId: "whatsapp-clone-96181",
+  storageBucket: "whatsapp-clone-96181.appspot.com",
+  messagingSenderId: "329761402472",
+  appId: "1:329761402472:web:d38b73caadda4cff3095dd",
+  measurementId: "G-JQ0SEJSHHL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
