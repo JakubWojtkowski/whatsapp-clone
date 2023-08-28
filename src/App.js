2c837177c6a6c5c9fdb7504fd40becd2ea26bcec
wojtko <j.w2001@onet.pl>
2023-08-28 18:18:34 +0200

    Create sidebar search

--- src/App.js
+++ src/App.js
@@ -18,13 +18,18 @@ export default App;
 const AppContainer = styled.div`
   display: grid;
   place-items: center;
-  background-color: #0c1317;
+  background: #0c1317;
   height: 100vh;
 `;
 
 const AppContent = styled.div`
   display: flex;
   height: 95vh;
-  width: 95vw;
-  box-shadow: -1px 4px 20px -6px rgba(0, 0, 0, 0.7);
+  width: 97.5vw;
+  box-shadow: -1px 4px 10px -6px rgba(0, 0, 0, 0.7);
+
+  @media only screen and (max-width: 1024px) {
+    height: 100vh;
+    width: 100vw;
+  }
 `;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,21 +5,42 @@ import GroupsIcon from "@mui/icons-material/Groups";
 import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 import ChatIcon from "@mui/icons-material/Chat";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { IconButton } from "@mui/material";
+import { FilterList, SearchOutlined } from "@mui/icons-material";
 
 function Sidebar() {
   return (
     <Container>
       <SidebarHeader>
-        <AccountCircleIcon />
+        <IconButton>
+          <AccountCircleIcon />
+        </IconButton>
+
         <SidebarHeaderRight>
-          <GroupsIcon />
-          <DonutLargeIcon />
-          <ChatIcon />
-          <MoreVertIcon />
+          <IconButton>
+            <GroupsIcon />
+          </IconButton>
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </SidebarHeaderRight>
       </SidebarHeader>
 
-      <SidebarSearch></SidebarSearch>
+      <SidebarSearch>
+        <SidebarSearchContent>
+          <SearchOutlined />
+          <SidebarInput placeholder="Search or start new chat" type="text" />
+        </SidebarSearchContent>
+        <IconButton>
+          <FilterList />
+        </IconButton>
+      </SidebarSearch>
 
       <SidebarChats></SidebarChats>
     </Container>
@@ -29,13 +50,66 @@ function Sidebar() {
 export default Sidebar;
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 0.35;
   background: #202c33;
+
+  .MuiSvgIcon-root {
+    color: lightgrey;
+  }
 `;
 
-const SidebarHeader = styled.div``;
+const SidebarHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+`;
 
-const SidebarHeaderRight = styled.div``;
+const SidebarHeaderRight = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-width: 10vw;
 
-const SidebarSearch = styled.div``;
+  .MuiSvgIcon-root {
+    margin-right: 2px;
+    font-size: 24px !important;
+  }
+`;
 
-const SidebarChats = styled.div``;
+const SidebarSearch = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 39px;
+  padding: 10px;
+
+  .MuiSvgIcon-root {
+    color: grey;
+  }
+`;
+
+const SidebarSearchContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid #f0f0f0;
+  border-radius: 8px;
+  padding: 4px 16px;
+`;
+
+const SidebarInput = styled.input`
+  background-color: #202c33;
+  border: none;
+  margin-left: 12px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const SidebarChats = styled.div`
+  background: #111b21;
+  flex: 1;
+`;
